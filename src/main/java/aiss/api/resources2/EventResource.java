8313e9a96edfe7c80cb454518f664197abe203f8
miggonort1 <miggonort1@alum.us.es>
2022-05-20 20:03:07 +0200

    commit

--- src/main/java/aiss/api/resources2/EventResource.java
+++ src/main/java/aiss/api/resources2/EventResource.java
@@ -1,62 +1,130 @@
 package aiss.api.resources2;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+import aiss.model2.Event;
+
 public class EventResource {
 	
-	//SONG COPIADO
-	
-	private String uri = "http://playlist-api.appspot.com/api/songs";
-	//private String uri = "http://localhost:8095/api/songs";
+		
+	private String uri = "https://city-events-api.ew.r.appspot.com/api/events";
 
 	
-	public Collection<Song> getAll() {
+	public Collection<Event> getEvents() {
 		ClientResource cr = null;
-		Song [] songs = null;
+		Event [] events = null;
 		try {
 			cr = new ClientResource(uri);
-			songs = cr.get(Song[].class);
+			events = cr.get(Event[].class);
 			
 		} catch (ResourceException re) {
-			System.err.println("Error when retrieving all songs: " + cr.getResponse().getStatus());
+			System.err.println("Error when retrieving all events: " + cr.getResponse().getStatus());
 			throw re;
 		}
 		
-		return Arrays.asList(songs);
+		return Arrays.asList(events);
 	}
 	
 
-	public Song getSong(String songId) {
-		//TODO
-		return null;
+	public Event getEvent(String eventId) {
+		ClientResource cr = null;
+		Event list = null;
+		try {
+			cr = new ClientResource(uri + "/" + eventId);
+			list = cr.get(Event.class);
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving the event: " + cr.getResponse().getStatus());
+		}
+		
+		return list;
 	}
 	
 
-	public Song addSong(Song song) {
-		// TODO
-		return null;
+	public Event addEvent(Event ev) {
+		ClientResource cr = null;
+		Event resultEvent = null;
+		try {
+			cr = new ClientResource(uri);
+			cr.setEntityBuffering(true);		// Needed for using RESTlet from JUnit tests
+			resultEvent = cr.post(ev,Event.class);
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when adding the event: " + cr.getResponse().getStatus());
+		}
+		
+		return resultEvent;
 
 	}
 	
-	public boolean updateSong(Song song) {
-		// TODO
-		return false;
-	}
-	
-
-	public boolean deleteSong(String songId) {
+	public boolean updateEvent(Event ev) {
 		ClientResource cr = null;
 		boolean success = true;
 		try {
-			cr = new ClientResource(uri + "/" + songId);
+			cr = new ClientResource(uri);
+			cr.setEntityBuffering(true);		// Needed for using RESTlet from JUnit tests
+			cr.put(ev);
+			
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when updating the event: " + cr.getResponse().getStatus());
+			success = false;
+		}
+		
+		return success;
+	}
+	
+
+	public boolean deleteEvent(String eventId) {
+		ClientResource cr = null;
+		boolean success = true;
+		try {
+			cr = new ClientResource(uri + "/" + eventId);
 			cr.setEntityBuffering(true);		// Needed for using RESTlet from JUnit tests
 			cr.delete();
 			
 		} catch (ResourceException re) {
-			System.err.println("Error when deleting the song: " + cr.getResponse().getStatus());
+			System.err.println("Error when deleting the event: " + cr.getResponse().getStatus());
 			success = false;
 			throw re;
 		}
 		
 		return success;
 		
+	}
+	
+	
+	public boolean addFqaToEvent(String eventID, String fqaID) {
+		ClientResource cr = null;
+		boolean exito = true;
+		try {
+			cr = new ClientResource(uri+"/"+eventID+"/"+fqaID);
+			cr.setEntityBuffering(true);
+			cr.post(" ");
+		} catch (ResourceException e) {
+			System.err.println("Error a intentar añadir la fqa al evento: "+cr.getResponse().getStatus());
+			exito=false;
+			throw e;
+		}
+		return exito;
+	}
+	
+	public boolean deleteFqaFromEvent(String eventID, String fqaID) {
+		ClientResource cr = null;
+		boolean exito = true;
+		try {
+			cr = new ClientResource(uri+"/"+eventID+"/"+fqaID);
+			cr.setEntityBuffering(true);
+			cr.delete();
+		} catch (ResourceException e) {
+			System.err.println("Error a intentar eliminar la fqa al evento: "+cr.getResponse().getStatus());
+			exito=false;
+			throw e;
+		}
+		return exito;
 	}
 }
