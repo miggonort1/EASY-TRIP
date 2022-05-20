8a48bc0156a00f5082c4b42f327be282a1cb8cf3
enrpermil <enrpermil@alum.us.es>
2022-05-20 18:18:24 +0200

    City , Event, Fqas

---
+++ src/main/java/aiss/api/resources2/Fqas.java
@@ -0,0 +1,64 @@
+package aiss.api.resources2;
+
+
+public class Fqas {
+	
+	//SONG COPIADO
+	
+	private String uri = "http://playlist-api.appspot.com/api/songs";
+	//private String uri = "http://localhost:8095/api/songs";
+
+	
+	public Collection<Song> getAll() {
+		ClientResource cr = null;
+		Song [] songs = null;
+		try {
+			cr = new ClientResource(uri);
+			songs = cr.get(Song[].class);
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving all songs: " + cr.getResponse().getStatus());
+			throw re;
+		}
+		
+		return Arrays.asList(songs);
+	}
+	
+
+	public Song getSong(String songId) {
+		//TODO
+		return null;
+	}
+	
+
+	public Song addSong(Song song) {
+		// TODO
+		return null;
+
+	}
+	
+	public boolean updateSong(Song song) {
+		// TODO
+		return false;
+	}
+	
+
+	public boolean deleteSong(String songId) {
+		ClientResource cr = null;
+		boolean success = true;
+		try {
+			cr = new ClientResource(uri + "/" + songId);
+			cr.setEntityBuffering(true);		// Needed for using RESTlet from JUnit tests
+			cr.delete();
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when deleting the song: " + cr.getResponse().getStatus());
+			success = false;
+			throw re;
+		}
+		
+		return success;
+		
+	}
+
+}
