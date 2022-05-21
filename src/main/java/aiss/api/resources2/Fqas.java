a99d19abf3c9596467225fb3429ee2ad87f0bfa2
enrpermil <enrpermil@alum.us.es>
2022-05-21 12:52:52 +0200

    City Resource, Event Resource, Fqas

--- src/main/java/aiss/api/resources2/Fqas.java
+++ src/main/java/aiss/api/resources2/Fqas.java
@@ -1,58 +1,97 @@
 package aiss.api.resources2;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
 
 public class Fqas {
 	
-	//SONG COPIADO
 	
-	private String uri = "http://playlist-api.appspot.com/api/songs";
-	//private String uri = "http://localhost:8095/api/songs";
+	private String uri = "https://city-events-api.ew.r.appspot.com/api/fqas";
 
 	
-	public Collection<Song> getAll() {
+	public Collection<Fqas> getAll() {
 		ClientResource cr = null;
-		Song [] songs = null;
+		Fqas [] fqas = null;
 		try {
 			cr = new ClientResource(uri);
-			songs = cr.get(Song[].class);
+			fqas = cr.get(Fqas[].class);
 			
 		} catch (ResourceException re) {
-			System.err.println("Error when retrieving all songs: " + cr.getResponse().getStatus());
+			System.err.println("Error when retrieving all Fqas: " + cr.getResponse().getStatus());
 			throw re;
 		}
 		
-		return Arrays.asList(songs);
+		return Arrays.asList(fqas);
 	}
 	
 
-	public Song getSong(String songId) {
-		//TODO
-		return null;
+	public Fqas getFqa(String fqaId) {
+		
+		ClientResource cr = null;
+		Fqas list = null;
+		try {
+			cr = new ClientResource(uri + "/" + fqaId);
+			list = cr.get(Fqas.class);
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving the fqa: " + cr.getResponse().getStatus());
+		}
+		
+		return list;
 	}
 	
 
-	public Song addSong(Song song) {
-		// TODO
-		return null;
+	public Fqas addFqa(Fqas f) {
+		
+		ClientResource cr = null;
+		Fqas resultFqa = null;
+		try {
+			cr = new ClientResource(uri);
+			cr.setEntityBuffering(true);		
+			resultFqa = cr.post(f,Fqas.class);
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when adding the fqa: " + cr.getResponse().getStatus());
+		}
+		
+		return resultFqa;
+		
 
 	}
 	
-	public boolean updateSong(Song song) {
-		// TODO
-		return false;
-	}
-	
-
-	public boolean deleteSong(String songId) {
+	public boolean updateFqa(Fqas fqa) {
+		
 		ClientResource cr = null;
 		boolean success = true;
 		try {
-			cr = new ClientResource(uri + "/" + songId);
-			cr.setEntityBuffering(true);		// Needed for using RESTlet from JUnit tests
+			cr = new ClientResource(uri);
+			cr.setEntityBuffering(true);		
+			cr.put(fqa);
+			
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when updating the fqa: " + cr.getResponse().getStatus());
+			success = false;
+		}
+		
+		return success;
+	}
+	
+
+	public boolean deleteFqa (String fqaId) {
+		
+		ClientResource cr = null;
+		boolean success = true;
+		try {
+			cr = new ClientResource(uri + "/" + fqaId);
+			cr.setEntityBuffering(true);	
 			cr.delete();
 			
 		} catch (ResourceException re) {
-			System.err.println("Error when deleting the song: " + cr.getResponse().getStatus());
+			System.err.println("Error when deleting the fqa: " + cr.getResponse().getStatus());
 			success = false;
 			throw re;
 		}
